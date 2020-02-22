64162447b6f316406153f87b7abe12b557ee293d
unknown <seglifabian@gmail.com>
2020-02-22 16:54:55 -0600

    React Router Finalized

--- src/Router.js
+++ src/Router.js
@@ -3,17 +3,14 @@ import { Switch, Route } from 'react-router'
 import Home from './components/Home'
 import About from './components/About'
 import Car from './components/Car'
-import { BrowserRouter } from 'react-router-dom'
 
 const Router = () => {
     return (
-        <BrowserRouter>
-      <Switch>
-        <Route exact path="/"component={Home} />
-        <Route path="/about"component={About} />
-        <Route path="/car/:id"component={Car} />
-      </Switch>
-      </BrowserRouter>
+            <Switch>
+              <Route exact path="/"component={Home} />
+              <Route path="/about"component={About} />
+              <Route path="/car/:id"component={Car} />
+            </Switch>
     );
 };
 
